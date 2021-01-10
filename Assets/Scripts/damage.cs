d0694b361ef82292ac9ab950dd0434c9d9ee6830
Alp <besiktasalp1903@gmail.com>
2021-01-10 11:41:22 +0300

    One of the spinies damaging properly and mario doesn’t pass through him

--- Assets/Scripts/damage.cs
+++ Assets/Scripts/damage.cs
@@ -8,19 +8,6 @@ public class damage : MonoBehaviour
     
     public int Hurt = 1;
 
-    /*
-    private void OnTriggerEnter(Collider collision)
-    {
-        Debug.Log("Someone touched me");
-        if (collision.gameObject.tag == "Player")
-        {
-            Debug.Log("Player touched me");
-            Vector3 damageDirection = collision.transform.position + transform.position;
-            damageDirection = damageDirection.normalized;
-            FindObjectOfType<playerhealth>().DamagePlayer(Hurt, damageDirection);
-        }
-    }
-    */
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
